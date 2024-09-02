bb9327de6943753104137580e08a960f21e2ae5b
grintzdel <mathis.oudin@etu.uca.fr>
2024-09-02 02:06:15 +0200

    :lipstick: Updating Layout for header and footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,23 +4,30 @@ import { DM_Sans } from "next/font/google";
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 const dmSans = DM_Sans({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-dm-sans'
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-dm-sans"
 });
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className={dmSans.variable}>
-      <body>
-      {children}
-      <PrismicPreview repositoryName={repositoryName} />
-      </body>
+    <body className="bg-[#070815] text-white">
+      <Header />
+        <main>
+          {children}
+        </main>
+      <Footer />
+    <PrismicPreview repositoryName={repositoryName} />
+    </body>
     </html>
   );
 }
