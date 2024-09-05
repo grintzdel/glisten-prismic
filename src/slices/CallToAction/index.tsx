31adcc8e65bff5e2ee4cc7f497b1af447e1ff371
grintzdel <mathis.oudin@etu.uca.fr>
2024-09-05 16:28:41 +0200

    :lipstick: CallToAction slice

--- src/slices/CallToAction/index.tsx
+++ src/slices/CallToAction/index.tsx
@@ -1,5 +1,9 @@
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicText, SliceComponentProps } from "@prismicio/react";
+import PlainLogo from "@/slices/CallToAction/PlainLogo";
+import Bounded from "@/components/Bounded";
+import { RichTextField } from "@prismicio/types";
+import ButtonLink from "@/components/ButtonLink";
 
 /**
  * Props for `CallToAction`.
@@ -11,13 +15,28 @@ export type CallToActionProps = SliceComponentProps<Content.CallToActionSlice>;
  */
 const CallToAction = ({ slice }: CallToActionProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="relative py-32 text-center font-medium md:py-40"
     >
-      Placeholder component for call_to_action (variation: {slice.variation})
-      Slices
-    </section>
+
+      <div
+        className="glow absolute -z-10 aspect-square w-full max-w-sm rounded-full bg-blue-500/50 blur-[160px] filter" />
+
+      <div className="glass-container rounded-lg bg-gradient-to-b from-slate-800 to-slate-900 p-4 md:rounded-xl">
+        <PlainLogo />
+      </div>
+
+      <div className="mt-8 max-w-xl text-5xl text-balance">
+        <PrismicText field={slice.primary.heading as RichTextField} />
+      </div>
+
+      <ButtonLink field={slice.primary.button_link} className="mt-6">
+        {slice.primary.button_text || "Learn More"}
+      </ButtonLink>
+
+    </Bounded>
   );
 };
 
